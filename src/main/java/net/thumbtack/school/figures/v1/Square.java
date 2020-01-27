f0d6c398fd6e0b545f009e8d306f8baeaab5a9f5
Dmitriy Petukhov <petuhovdn77112@mail.ru>
2020-01-27 19:42:52 +0600

    Solutions of the tasks in square theme

---
+++ src/main/java/net/thumbtack/school/figures/v1/Square.java
@@ -0,0 +1,118 @@
+package net.thumbtack.school.figures.v1;
+
+import java.util.Objects;
+
+public class Square {
+    private int xLeft;
+    private int yTop;
+    private int size;
+
+    public Square(Point leftTop, int size) {
+        this.xLeft = leftTop.getX();
+        this.yTop = leftTop.getY();
+        this.size = size;
+    }
+
+    public Square(int xLeft, int yTop, int size) {
+        this.xLeft = xLeft;
+        this.yTop = yTop;
+        this.size = size;
+    }
+
+    public Square(int size) {
+        xLeft = 0;
+        yTop = -size;
+        this.size = size;
+    }
+
+    public Square() {
+        this.xLeft = 0;
+        this.yTop = -1;
+        this.size = 1;
+    }
+
+    public Point getTopLeft() {
+        return new Point(xLeft, yTop);
+    }
+
+    public Point getBottomRight() {
+        return new Point(xLeft + size, yTop + size);
+    }
+
+    public void setTopLeft(Point topLeft) {
+        xLeft = topLeft.getX();
+        yTop = topLeft.getY();
+    }
+
+    public int getLength() {
+        return size;
+    }
+
+    public void moveTo(int x, int y) {
+        xLeft = x;
+        yTop = y;
+    }
+
+    public void moveTo(Point point) {
+        xLeft = point.getX();
+        yTop = point.getY();
+    }
+
+    public void moveRel(int dx, int dy) {
+        xLeft += dx;
+        yTop += dy;
+    }
+
+    public void resize(double ratio) {
+        size *= ratio;
+    }
+
+    public double getArea() {
+        return Math.pow(size, 2);
+    }
+
+    public double getPerimeter() {
+        return size * 4;
+    }
+
+    public boolean isInside(int x, int y) {
+        return x <= xLeft + size && x >= xLeft && y <= yTop + size && y >= yTop;
+    }
+
+    public boolean isInside(Point point) {
+        return point.getX() <= xLeft + size
+                && point.getX() >= xLeft
+                && point.getY() <= yTop + size
+                && point.getY() >= yTop;
+    }
+
+    public boolean isIntersects(Square square) {
+        for (int i = square.xLeft; i <= square.xLeft + size; i++) {
+            for (int j = square.yTop; j <= square.yTop + size; j++) {
+                if (isInside(i, j)) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isInside(Square square) {
+        return square.getTopLeft().getX() >= xLeft
+                && square.getTopLeft().getY() >= yTop
+                && size > square.size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Square square = (Square) o;
+        return xLeft == square.xLeft &&
+                yTop == square.yTop &&
+                size == square.size;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xLeft, yTop, size);
+    }
+}
